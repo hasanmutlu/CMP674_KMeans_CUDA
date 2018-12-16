78f98aedc8b970e1cefc8c8389abf976f10c08a0
Hasan MUTLU <hasanmutlu9@gmail.com>
2018-12-16 11:22:27 +0300

    device functions are updated

--- Kmeans_CUDA/Kmeans/Kmeans/kernel.cu
+++ Kmeans_CUDA/Kmeans/Kmeans/kernel.cu
@@ -6,7 +6,6 @@
 
 #define int_ptr int*
 #define float_ptr float*
-#define list_ptr float**
 #define X(point) point[0] // returns X of given point
 #define Y(point) point[1] // returns Y of given point
 #define POINT(list,i) &(list[i]) //returns i. point from given list
@@ -37,27 +36,21 @@ __global__ void generate_rand_kernel(curandState *state, int sampleCount, float_
 #pragma endregion
 
 //Get distance of point to given Centroid
-__device__ double get_distance_to_centroid(float_ptr centroid, int_ptr point)
+__device__ float get_distance_to_centroid(float_ptr centroid, float_ptr point)
 {
-	int point_x = X(point);
-	int point_y = Y(point);
-	float centroid_x = X(centroid);
-	float centroid_y = Y(centroid);
-	float diffX = point_x - centroid_x;
-	float diffY = point_y - centroid_y;
+	float diffX = X(point) - X(centroid);
+	float diffY = Y(point) - Y(centroid);
 	return sqrtf(diffX * diffX + diffY * diffY);
 }
 
 //Find centroid which distance is minumum to given point 
-__device__ int get_min_distance_centroid(float_ptr centroids , int_ptr point, int centroid_count)
+__device__ int get_closest_centroid(float_ptr centroids , float_ptr point, int centroid_count)
 {
-	int point_x = X(point);
-	int point_y = Y(point);
 	int min = 0;
 	float min_value = 0;
-	for (int i=0 ; i< centroid_count; i+=2)
+	for (int i=0 ; i< SIZE(centroid_count); i+=2)
 	{
-		float distance = get_distance_to_centroid(centroids + i, point);
+		float distance = get_distance_to_centroid(POINT(centroids,i), point);
 		if (min == 0 || distance < min_value)
 		{
 			min = i / 2;
@@ -67,29 +60,29 @@ __device__ int get_min_distance_centroid(float_ptr centroids , int_ptr point, in
 	return min;
 }
 
-//recenter given centroid according to points of it
-__device__ float_ptr recenter_centroid(float_ptr centroid, int_ptr points, int point_count)
+//recenter given centroid according to points of it.
+__global__ void recenter_centroid(float_ptr points, int point_count,int_ptr centroidPointMap, float_ptr centroids, int_ptr centroidPointCount)
 {
-	float result[2];
-	X(result) = 0;
-	Y(result) = 0;
-	for (int i=0 ; i<point_count *2 ; i+=2)
+	int id = threadIdx.x + blockIdx.x * blockDim.x;
+	if (id < point_count)
 	{
-		X(result) += points[i] / point_count;
-		Y(result) += points[i + 1] / point_count;
+		float_ptr point = POINT(points, id*2);
+		float_ptr centroid = POINT(centroids, centroidPointMap[id] * 2);
+		int pointCount = centroidPointCount[centroidPointMap[id]];
+		X(centroid) += X(point) / pointCount;
+		Y(centroid) += Y(point) / pointCount;
 	}
-	return result;
 }
 
-__global__ void kmeans(int_ptr points, int_ptr result )
+__global__ void kmeans(float_ptr points, int point_count, float_ptr centroids, int centroidCount, int_ptr centroidPointMap, int_ptr centroidPointCount)
 {
-	int point_count = 4096;
-	int centroid_count = 5;
-	for (int i=0;i<point_count *2 ; i+=2)
+	int id = threadIdx.x + blockIdx.x * blockDim.x;
+	if (id < point_count)
 	{
-
-
-
+		float_ptr point = POINT(points, id * 2);
+		int closest_centroid = get_closest_centroid(centroids, point, centroidCount);
+		centroidPointMap[id] = closest_centroid;
+		centroidPointCount[closest_centroid]++;
 	}
 }
 
@@ -145,33 +138,34 @@ bool isCentroidChanged(float_ptr curCentroid, float_ptr prevCentroid)
 	return X(curCentroid) != X(prevCentroid) || Y(curCentroid) != Y(prevCentroid);
 }
 
-bool checkProcessCompleted(float_ptr cur_centroids, float_ptr prev_centroids, int count)
+bool isProcessCompleted(float_ptr cur_centroids, float_ptr prev_centroids, int count)
 {
 	bool result = true;
 	for (int i=0;i<SIZE(count);i+=2)
 	{
 		float_ptr c_centroid = POINT(cur_centroids, i);
 		float_ptr p_centroid = POINT(prev_centroids, i);
-		if (isCentroidChanged(c_centroid, p_centroid))
+		if (isCentroidChanged(c_centroid, p_centroid) == true)
 		{
-
+			result = false;
+			break;
 		}
-
-
 	}
 	return result;
-
 }
 
-void createCentroidsPoints(int centroidCount, int pointCount, list_ptr &result)
+
+void cudaKmeansProcess(float_ptr points, int pointCount, float_ptr centroids, int centroidCount, int_ptr &centroidPointMap, int_ptr &centroidPointCount)
 {
-	result = reinterpret_cast<float_ptr*>(calloc(centroidCount, sizeof(float_ptr)));
-	for (int i=0;i<centroidCount;i++)
-	{
-		result[i] = reinterpret_cast<float_ptr>(calloc(SIZE(pointCount),sizeof(float)));
-	}
+
+
 }
 
+void cudaRecenterCentroids(float_ptr points, int pointCount, float_ptr &centroids, int centroidCount, int_ptr centroidPointMap, int_ptr centroidPointCount)
+{
+
+
+}
 
 void cudaKmeans()
 {
@@ -180,51 +174,20 @@ void cudaKmeans()
 	int pointCount = 4096;
 	float_ptr points;
 	float_ptr centroids;
-	list_ptr centroidPoints;
-	int_ptr pointCountOfCentroids = new int[centroidCount];
-	for (int i = 0; i<centroidCount;i++)
-	{
-		pointCountOfCentroids[i] = pointCount;
-	}
+	float_ptr prev_centroids = new float[SIZE(centroidCount)];
+	int_ptr centroidPointMap = reinterpret_cast<int_ptr>(calloc(pointCount, sizeof(int)));
+	int_ptr centroidPointCount = reinterpret_cast<int_ptr>(calloc(centroidCount, sizeof(int)));
 	cudaGetRandomPoints(pointCount, points);
 	cudaGetRandomPoints(centroidCount, centroids);
-	createCentroidsPoints(centroidCount, pointCount, centroidPoints);
-	for (int i=0;i<centroidCount;i++)
-	{
-		print_points(centroidPoints[i],pointCount);
-		system("pause");
-
-	}
 	bool found = false;
 	while (found == false)
 	{
-
-		//cudaKmeansProcess(points, pointCount, centroids, centroidCount);
-		//cudaRecenterCentroids()
-		//found = is
-		found = true;
+		memcpy(prev_centroids, centroids, sizeof(float)*SIZE(centroidCount));
+		cudaKmeansProcess(points, pointCount, centroids, centroidCount, centroidPointMap,centroidPointCount);
+		cudaRecenterCentroids(points, pointCount, centroids, centroidCount, centroidPointMap,centroidPointCount);
+		found = isProcessCompleted(centroids, prev_centroids,centroidCount);
 	}
-
-
-
-	/*
-	hostResult = new int[centroidCount * 2];
-	
-	cudaMalloc((void**)devPoints, pointCount * 2 * sizeof(int));
-	cudaMemcpy(devPoints, points, pointCount * 2 * sizeof(int),cudaMemcpyHostToDevice);
-
-	cudaMalloc((void**)devResult, centroidCount * 2 * sizeof(int));
-	cudaMemset(devResult, 0 , centroidCount * 2 * sizeof(int));
-
-	//kmeans << <64, 64 >> > (devPoints, devResult);
-	cudaMemcpy(hostResult, devResult, centroidCount * 2 * sizeof(int),cudaMemcpyDeviceToHost);
-	cudaFree(devPoints);
-	cudaFree(devResult);
 	cudaDeviceReset();
-	*/
-
-
-
 }
 
 
